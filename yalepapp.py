2bb93e6a9cf79d9dc94ef5d54dc164f573ee3406
Jamie Yi <jamiemyi@gmail.com>
2022-10-30 19:40:36 -0400

    rating stuff

--- yalepapp.py
+++ yalepapp.py
@@ -40,21 +40,15 @@ def building_details():
     name = request.args.get('name')
     building = get_buildings_by_name(name)[0]
     building_info = building.to_tuple()
-
-    html = ''
-    pattern = '<h5>%s</h5><strong>%s</strong>'
-    html += pattern % ('Name', building_info[0])
-    html += pattern % ('Address', building_info[1])
-    html += pattern % ('Rating', building_info[2])
-
-    #5 star system
-    html += '<h6>Rate this Building</h6>'
-    star_pattern = '<button onclick="changeRating(%s)">%s</button>'
-    for i in range(0,5):
-        html += star_pattern % (i+1, i+1)
-
+    html = render_template('building.html', name=building_info[0], 
+        address=building_info[1], rating=building_info[2])
     response = make_response(html)
     return response
+
+
+# @app.route('/info', methods=['GET'])
+# def build():
+    
 
 
     
