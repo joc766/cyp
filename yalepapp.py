c3eb73d9069469389ee6ef2cc1db31c07f3443b6
Jack O'Connor <j.oconnor@yale.edu>
2022-11-11 12:16:02 -0500

    login and register functions complete. Endpoints still need to be verified

--- yalepapp.py
+++ yalepapp.py
@@ -1,6 +1,10 @@
-from flask import Flask, request, make_response, redirect, url_for, render_template
+from flask import Flask, request, make_response, redirect, url_for, render_template, session
 from flask import render_template
-from helpers import get_buildings_by_name, update_rating
+from werkzeug.security import generate_password_hash
+
+from helpers import get_buildings_by_name, update_rating, verify_login
+from decorators import login_required
+from database.models.user import User
 
 #we are using jinja
 #-----------------------------------------------------------------------
@@ -15,6 +19,58 @@ def index():
     html = render_template('index.html')
     response = make_response(html)
     return response
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html') # TODO write login html
+
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        if not username:
+            raise KeyError('no username given') # TODO write an error page
+        password = request.form.get('password')
+        if not password:
+            raise KeyError('no password given')
+        
+        try:
+            user_id = verify_login(username, password)
+        except KeyError as e:
+            # username invalid
+            raise(e)
+        except ValueError as e:
+            # password invalid
+            raise(e)
+        
+        session['user_id'] = user_id
+        return redirect('/')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+
+    elif request.method == 'POST':
+        password = request.form.get('password')
+        username = request.form.get('username')
+        first_name = request.form.get('firstname')
+        last_name = request.form.get('lastname')
+        year = request.form.get('year')
+        college = request.form.get('college')
+        pwd_hash = generate_password_hash(password)
+
+        new_user = User(pwd_hash, username, first_name, last_name, year, college)
+
+        try:
+            new_user.insert_into_db()
+
+        except Exception as e:
+            raise(e)
+
+        
+
+
 
 
 @app.route('/search', methods=['GET'])
