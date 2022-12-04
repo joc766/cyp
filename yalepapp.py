0780f1565dea381191766ce7cbaf066f39004ab6
Jack O'Connor <j.oconnor@yale.edu>
2022-12-04 15:44:50 -0500

    much better image service

--- yalepapp.py
+++ yalepapp.py
@@ -1,13 +1,11 @@
-import os
-
-from flask import Flask, request, make_response, redirect, url_for, render_template, session, jsonify, send_from_directory
+from flask import Flask, request, make_response, redirect, url_for, render_template, session, jsonify, send_file
 from flask import render_template
 from helpers import get_buildings_by_name, update_rating, get_building_reviews, get_comments_keyword, add_review, vote_for_review, get_votes, get_user, get_user_reviews, get_buildings_by_tag, get_user_comments
 from werkzeug.security import generate_password_hash
 from werkzeug.exceptions import HTTPException, NotFound
 from werkzeug.utils import secure_filename
 
-from helpers import get_buildings_by_name, update_rating, verify_login, insert_into_db, upload_image_to_db
+from helpers import get_buildings_by_name, update_rating, verify_login, insert_into_db, upload_image_to_db, get_image
 from decorators import login_required
 from database.models.user import User
 from datetime import datetime
@@ -17,13 +15,13 @@ from tempfile import mkdtemp
 #we are using jinja
 #-----------------------------------------------------------------------
 
-UPLOAD_FOLDER = mkdtemp(dir='static/user_images')
+# UPLOAD_FOLDER = 'static/user_images'
 
 app = Flask(__name__, template_folder='templates')
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+# app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 Session(app)
 
 #-----------------------------------------------------------------------
@@ -175,16 +173,15 @@ def vote():
 @app.route('/uploadImage', methods=['POST'])
 def upload_image():
     file = request.files["img"]
-    filename = secure_filename(file.filename)
-    file_location = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-    file.save(file_location)
-    id = upload_image_to_db(file_location, filename)
-    response = make_response({"src": f"imageServe/{filename}", "img_id": id, "filename": filename})
+    id = upload_image_to_db(file)
+    response = make_response({"img_id": id, "src": f"imageServe/{id}"})
+    print("HERE")
     return response
 
-@app.route('/imageServe/<filename>', methods=["GET"])
-def send_image(filename):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+@app.route('/imageServe/<img_id>', methods=["GET"])
+def send_image(img_id):
+    img = get_image(img_id)
+    return send_file(img, mimetype='image/*')
 
 @app.route('/submitReview', methods=['POST'])
 def submit_comment():
